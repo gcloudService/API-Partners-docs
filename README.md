0f0b66a13ddfa244e857e69e9ddd003651d2ac42
sanghaklee <code.ryan.lee@gmail.com>
2018-03-05 20:40:08 +0900

    실서버 환경에 맞게 수정

---
+++ README.md
@@ -0,0 +1,18 @@
+# gcloud-partner-api-docs
+
+cpapi.gabia.com/docs
+
+## Usage
+```
+$ npm start
+```
+
+> Docs on https://cpapi.gabia.com/docs
+
+> Internal server port: **5000**
+
+## Dev Server
+```
+$ npm start:dev
+```
+> Internal server port: **9000**
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use(morgan('combined', {
+	// skip: function (req, res) { return res.statusCode >= 300 } //
+}))
+
+app.use('/', express.static(__dirname))
+app.listen(PORT, function() { console.log(`listening on PORT. ${PORT}.`, __dirname) })
